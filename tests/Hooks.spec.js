2acc1f8f70f8bd6922bb0905ad20c4489861d44a
chitraabanu <chitraabanues@gmail.com>
2025-04-22 20:02:25 +0100

    modified

--- tests/Hooks.spec.js
+++ tests/Hooks.spec.js
@@ -3,55 +3,68 @@ let page;
 test.beforeEach('Before Each ', async ({browser}) =>{
 
     page=await browser.newPage()
-    await page.goto('https://demoblaze.com/')
-    await page.locator('#login2').click();
-    await page.locator('#loginusername').fill('pavanol')
-    await page.locator('#loginpassword').fill('test@123')
-    await page.locator("//button[text()='Log in']").click()
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('#user-name').fill('standard_user')
+    await page.locator('#password').fill('secret_sauce')
+    await page.locator("#login-button").click()
     await page.waitForTimeout(3000);
 })
-test.afterEach('After each', async ({page}) =>{
-    if(!page.closed?.())
+test.afterEach('After each', async () =>{
+    /*try
     {
-    
-    try{
-    await page.waitForSelector('#logout2',{state:'visible'});
-    await page.locator('#logout2').click();
-    await page.waitForTimeout(3000)
+        if(page.isClosed())
+        {
+            console.warn('page already closed skipping logout');
+            
+        }
+
+     const burger_menu=await page.locator('#react-burger-menu-btn');
+     const sidebar_menu=await page.locator("//a[text()='Logout']");
+     if(await burger_menu.isVisible({timeout:5000}))
+     {
+     await burger_menu.click();
+     await sidebar_menu.waitFor({state:'visible',timeout:4000});
+     await sidebar_menu.click();
+     }
+     else
+     {
+        console.warn("Skipping logout");
+     }
     }
     catch(err)
     {
-        console.warn('logout failed',err.message);
-    }
-    }
-    else{
-        console.warn('page already closed skipping logout');
-    }
-    
-    
-})
-test.only('Home page' ,async ({page}) =>{
-  const products=await page.$$("//a[@class='hrefch']")
-  for (const product of products)
-  {
-   // if (await product.isVisible())
-      console.log('products are ',await product.textContent())
-   /* else 
-    {
-    console.log('Product element is no longer available');
+        console.log('logout failed in afterEach:', err.message);
     }*/
-  }
-
-  await page.waitForTimeout(3000)
+    await page.locator('#react-burger-menu-btn').click();
+    await page.locator("//a[text()='Logout']").click();
   
 })
-test('Add to Cart' , async ({page}) =>{
-    await page.locator("//a[@class='hrefch']").click()
-    await page.locator('//a[text()="Add to cart"]')
-    page.on('dialog' , async dialog =>{
-        expect(await dialog.message()).toHaveText('Product added.')
-        await dialog.accept()
+ test('products' ,async () =>{
+    page.on('dialog', async dialog =>{
+        expect(dialog.type()).toContain('alert')
+        expect(dialog.message()).toContain('Press the button')
+        expect(dialog.accept())
 
     })
-    await page.waitForTimeout(3000)
+    const products=await page.$$("//div[@class='inventory_list']//div[@class='inventory_item']//div[@class='inventory_item_description']//a//div");
+    for(const product of products)
+    {
+        const value=await product.textContent();
+        console.log(value);
+    }
+
+    
+  
+})
+ test('Add to cart', async()=>{
+    const product = await page.locator("//a[@id='item_4_title_link']/div");
+    await product.waitFor({state:'visible',time:3000})
+    await product.click();
+    const addToCart=await page.locator("//button[text()='Add to cart']");
+    await expect(addToCart).toBeVisible();
+    await addToCart.click({timeout:5000});
+    const item=await page.locator(".shopping_cart_link span");
+    await item.click();
+    await expect(item).toBeVisible();
+
 })
